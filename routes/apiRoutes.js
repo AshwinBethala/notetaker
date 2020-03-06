c7e6f312214c43e6565b9481bfc5b3c6e48399f7
Ashwin Bethala <57239378+AshwinBethala@users.noreply.github.com>
2020-03-05 23:31:32 -0600

    newapi
    I have improved the apiRoute.js even further.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,41 +1,46 @@
 var noteContests = require("../db/db.json");
+
 var fs = require("fs");
 
+var path = require("path");
+
 module.exports = function(app) {
-
+  
   app.get("/api/notes", function(req, res) {
+    
     res.json(noteContests);
+  
   });
-
+  
   app.post("/api/notes", function(req, res) {
-
+    
     const note = req.body
+    
     console.log(note);
-
+    
     noteContests.push(note);
+    
     console.log(noteContests)
     // noteContest write to db.json file// 
-
-    fs.writeFile(__dirname + "../db/db.json", JSON.stringify(noteContests), function(err) {
-
+   
+    console.log(__dirname);
+    
+    fs.writeFile(path.join(__dirname, "../db/db.json") , JSON.stringify(noteContests), function(err) {
+      
       if (err) {
         return res.send(err);
       }
-
       return res.json(true);
-
     });
-
-      
- 
   });
-
-
+ 
   app.delete("/api/notes/:id", function(req, res) {
-
+    
     tableData.length = 0;
+    
     waitListData.length = 0;
-
+    
     res.json({ ok: true });
+    
   });
 };
